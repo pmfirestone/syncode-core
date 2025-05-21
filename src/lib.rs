ad545b8e98c9a8837a0b30d56b954b55cbb2eb49
Preston M. Firestone <firestone.preston@gmail.com>
2025-05-21 19:33:59 +0900

    Improve documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,20 @@
 [SynCode](https://arxiv.org/abs/2403.01632) is a library for constrained
 generation. It forces an LLM to generate sequences that satisfy a given LR
 grammar.
+
+The interface presented to the user has two parts: a constructor and a masker.
+
+The constructor takes in a model vocabulary and a context-free grammar and
+returns a masker struct.
+
+That struct has a single public function that takes in an array of bytes, the
+model's generated output so far, and returns a mask, a boolean array, over the
+model's vocabulary. 
+
+Applying this mask to the model's vocabulary should, provided it was
+constructed with sufficient lookahead, prevent the model from ever going
+outside of the grammar.
+ 
 !*/
 pub mod mask;
 pub mod parser;
