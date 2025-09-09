7b4f91d8e7ff9adf072e458fa3562f81185f1def
Preston M. Firestone <firestone.preston@gmail.com>
2025-09-09 16:18:15 -0500

    Parallelizing this actually does make it faster.

--- examples/construct_mask_store.rs
+++ examples/construct_mask_store.rs
@@ -1,4 +1,5 @@
 // use syncode_core::mask::dfa_mask_store;
+use rayon::prelude::*;
 use syncode_core::bytes::restore_bytes;
 use tokenizers::Tokenizer;
 
@@ -6,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let tokenizer = Tokenizer::from_pretrained("Qwen/Qwen3-235B-A22B", None)?;
     let vocab = tokenizer.get_vocab(false);
     let tokens: Vec<&String> = vocab.keys().collect();
-    let byte_tokens: Vec<Vec<u8>> = tokens.iter().map(|t| restore_bytes(t)).collect();
+    let byte_tokens: Vec<Vec<u8>> = tokens.par_iter().map(|t| restore_bytes(t)).collect();
     println!("{:#?}", byte_tokens[32]);
     // let mask_store = dfa_mask_store();
     Ok(())
