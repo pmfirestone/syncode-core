68c5dd6faca054cfd7aa48f97cfd73dc00aade22
Preston M. Firestone <firestone.preston@gmail.com>
2025-09-29 13:55:23 -0700

    Add public top-level invocation.
    This is one of the basic points of the API.

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,41 @@
 #![doc = include_str!("../README.md")]
 #![feature(test)]
 
+use bytes::restore_bytes;
+use candle_core::ToUsize2;
+use grammar::EBNFParser;
+use mask::{DFAMaskStore, dfa_mask_store};
+use std::fs;
+use tokenizers::Tokenizer;
+use types::{Lexer, Parser};
+
 pub mod bytes;
 pub mod dfa;
+pub mod generate;
 pub mod grammar;
 pub mod lexer;
 pub mod mask;
 pub mod parser;
 pub mod table;
 pub mod types;
+
+pub fn mask_store(model_id: &str, grammar_file: &str) -> DFAMaskStore {
+    let tokenizer = Tokenizer::from_pretrained(model_id, None).unwrap();
+    let mut byte_tokens: Vec<Vec<u8>> = Vec::new();
+
+    // Make sure the tokens are in index order.
+    for (idx, _) in [0..tokenizer.get_vocab_size(false)].iter().enumerate() {
+        byte_tokens.push(restore_bytes(&tokenizer.id_to_token(idx as u32).unwrap()));
+    }
+
+    let Ok(grammar) = EBNFParser::new(&fs::read_to_string(grammar_file).unwrap(), "start").parse()
+    else {
+        panic!()
+    };
+
+    let Ok(parser) = Parser::new(&grammar.clone()) else {
+        panic!()
+    };
+
+    dfa_mask_store(&grammar.terminals, &byte_tokens, &parser, 2)
+}
